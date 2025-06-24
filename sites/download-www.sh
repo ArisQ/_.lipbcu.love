8d29986d1d93c78cff0ad8a6eaf8cef6a8139587
ArisQ <qiaoli.pg@gmail.com>
2025-06-24 17:33:58 +0800

    update www

---
+++ sites/download-www.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ev
+
+ARTIFACT_ID=$(curl -L -H "Authorization: Bearer $GITHUB_TOKEN" \
+	-H "Accept: application/vnd.github+json" \
+	https://api.github.com/repos/ArisQ/www.lipbcu.love/actions/artifacts |jq .artifacts[0].id)
+
+curl -L -H "Authorization: Bearer $GITHUB_TOKEN" \
+	-H "Accept: application/vnd.github+json" \
+	-o artifact.zip \
+	"https://api.github.com/repos/ArisQ/www.lipbcu.love/actions/artifacts/$ARTIFACT_ID/zip"
+
+unzip artifact.zip -d www
+
+rm artifact.zip
+
